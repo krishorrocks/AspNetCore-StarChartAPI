8eb98c9b3316d79cce4652719eb7f50ceabfa2a6
Kris Horrocks <45402725+krishorrocks@users.noreply.github.com>
2018-11-27 12:38:19 -0800

    Update CelestialObjectController.cs

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,19 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var foundObject = _context.CelestialObjects.Where(c => c.Id == id).FirstOrDefault();
+
+            if (foundObject == null)
+                return NotFound();
+            else
+            {
+                foundObject.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+                return Ok(foundObject);
+            }
+        }
     }
 }
